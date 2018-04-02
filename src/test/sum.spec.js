155548ab9dec9ed3c1720846f927c970fd4580f4
keep_wan <whevether@outlook.com>
2018-04-02 23:56:43 +0800

    update

--- src/test/sum.spec.js
+++ src/test/sum.spec.js
@@ -1,5 +1,7 @@
 const sum = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('adds 1 + 2 to equal 3', () => {
+  it("should import",()=>{
+    expect(sum(1, 2)).toBe(3);
+  });
 });
