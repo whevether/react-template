fe575da3afcb76cc6ccfbdf3094cdbc3c8e5dc5f
keep_wan <whevether@outlook.com>
2018-04-18 09:48:39 +0800

    update

--- src/components/keep.js
+++ src/components/keep.js
@@ -2,16 +2,20 @@ import React,{Component} from 'react';
 import { Helmet } from 'react-helmet';
 class Keep extends Component{
   static getDerivedStateFromProps(nextProps, prevState) {
+    /*eslint-disable no-console*/
     console.log(nextProps);
     console.log(prevState);
     // Called after a component is instantiated or before it receives new props.
     // Return an object to update state in response to prop changes.
     // Return null to indicate no change to state.
   }
-  static 
   constructor(props){
     super(props);
   }
+  // static getSnapshotBeforeUpdate(nextProps,prevState){
+  //   console.log(nextProps);
+  //   console.log(prevState);
+  // }
   head(){
     return(
       <Helmet>
