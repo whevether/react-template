f6b5120ce39458ab564e4fdc71e65e5c21414f9e
万文峰 <whevether@qq.com>
2018-09-20 17:40:41 +0800

    update

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,61 +1,61 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 // 服务端
-import AsyncHome from '../components/home';
-import AsyncRedBull from '../components/redBull';
-import AsyncSnacks from '../components/snacks'; //包裹了子路由的组件
-import AsyncNotFoundPage from '../components/nodeFound';
-import AsyncApp from '../containers/app';
-import AsyncKeep from '../components/keep';
+// import AsyncHome from '../components/home';
+// import AsyncRedBull from '../components/redBull';
+// import AsyncSnacks from '../components/snacks'; //包裹了子路由的组件
+// import AsyncNotFoundPage from '../components/nodeFound';
+// import AsyncApp from '../containers/app';
+// import AsyncKeep from '../components/keep';
 /*
   当不需要服务端渲染的时候可以开启代码分割。服务端渲染代码分割这个插件会有问题
 */ 
-// import Loadable from 'react-loadable'; 
-// // // 路由加载时动画 
-// const MyLoadingComponent = ({ isLoading, error }) => {
-//     // Handle the loading state
-//     if (isLoading) {
-//         return <div>玩命加载中......</div>;
-//     }
-//     // Handle the error state
-//     else if (error) {
-//         return <div>对不起。页面加载错误</div>;
-//     }
-//     else {
-//         return null;
-//     }
-// };
-// // //分割路由
-// const AsyncApp = Loadable({
-//   loader: ()=>import('../containers/app'),
-//   loading: MyLoadingComponent,
-//   delay: 200
-// });
-// const AsyncHome = Loadable({
-//     loader: () => import('../components/home'),
-//     loading: MyLoadingComponent,
-//     delay: 200
-// }); 
-// const AsyncRedBull = Loadable({
-//     loader: () => import('../components/redBull'),
-//     loading: MyLoadingComponent,
-//     delay: 200
-// });
-// const AsyncSnacks = Loadable({
-//     loader: () => import('../components/snacks'),
-//     loading: MyLoadingComponent,
-//     delay: 200
-// });
-// const AsyncKeep = Loadable({
-//   loader: () => import('../components/keep'),
-//   loading: MyLoadingComponent,
-//   delay: 200
-// });
-// const AsyncNotFoundPage = Loadable({
-//   loader: () => import('../components/nodeFound'),
-//   loading: MyLoadingComponent,
-//   delay: 200
-// });
+import Loadable from 'react-loadable'; 
+// // 路由加载时动画 
+const MyLoadingComponent = ({ isLoading, error }) => {
+    // Handle the loading state
+    if (isLoading) {
+        return <div>玩命加载中......</div>;
+    }
+    // Handle the error state
+    else if (error) {
+        return <div>对不起。页面加载错误</div>;
+    }
+    else {
+        return null;
+    }
+};
+// //分割路由
+const AsyncApp = Loadable({
+  loader: ()=>import('../containers/app'),
+  loading: MyLoadingComponent,
+  delay: 200
+});
+const AsyncHome = Loadable({
+    loader: () => import('../components/home'),
+    loading: MyLoadingComponent,
+    delay: 200
+}); 
+const AsyncRedBull = Loadable({
+    loader: () => import('../components/redBull'),
+    loading: MyLoadingComponent,
+    delay: 200
+});
+const AsyncSnacks = Loadable({
+    loader: () => import('../components/snacks'),
+    loading: MyLoadingComponent,
+    delay: 200
+});
+const AsyncKeep = Loadable({
+  loader: () => import('../components/keep'),
+  loading: MyLoadingComponent,
+  delay: 200
+});
+const AsyncNotFoundPage = Loadable({
+  loader: () => import('../components/nodeFound'),
+  loading: MyLoadingComponent,
+  delay: 200
+});
 export default [
     {
       component: AsyncApp,
