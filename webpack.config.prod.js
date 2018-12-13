c750a832186815acc4c235f53a46722ec4850451
万文峰 <whevether@qq.com>
2018-12-13 09:58:05 +0800

    css-loader 升级2优化压缩

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -214,8 +214,13 @@ export default {
           {
             loader: 'css-loader',
             options: {
-              minimize: true,
-              sourceMap: false
+              // minimize: true,
+              // sourceMap: false
+              importLoaders: 2,
+              modules: true,
+              // namedExport: true, // this is  invalid Options ,I find it
+              camelCase: true,
+              localIdentName: '[path][name]__[local]--[hash:base64:5]',
             }
           }, {
             loader: 'postcss-loader',
