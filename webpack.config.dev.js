54ad166d5b84cc99564d6e5fc0a8b28d7d2e688f
万文峰 <whevether@qq.com>
2019-03-13 13:39:08 +0800

    增加px转rem 单位插件

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -177,7 +177,7 @@ export default {
                 require('postcss-pxtorem')({
                   rootValue: 16,
                   unitPrecision: 5,
-                  propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
+                  propList: ['*'],
                   selectorBlackList: [],
                   replace: true,
                   mediaQuery: false,
