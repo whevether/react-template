0eaea3c0c7b5f4c00e92bb8e3cb2ce03ba9f2078
keep_wan <whevether@outlook.com>
2021-02-07 16:54:34 +0800

    更新依赖

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createBrowserHistory } from 'history';
-import { Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import configureStore from 'store/configureStore';
 //GYGBZ-MB5WR-DPZWK-WTKS2-UWBVS-VXBKW
 //服务端渲染的时候样式需要放在入口这里。放别的地方环境会报错; 
