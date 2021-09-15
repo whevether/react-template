00410c7347ff4b2e022f92e61a538f8d81aa2a44
keep_wan <whevether@outlook.com>
2021-09-16 00:03:00 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -33,16 +33,28 @@ const config = {
     filename: 'js/[name].js'
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
+    static: {
+      directory: path.join(__dirname, "public/"),
+      staticOptions: {},
+      // Don't be confused with `devMiddleware.publicPath`, it is `publicPath` for static directory
+      // Can be:
+      // publicPath: ['/static-public-path-one/', '/static-public-path-two/'],
+      publicPath: "/",
+      // Can be:
+      // serveIndex: {} (options for the `serveIndex` option you can find https://github.com/expressjs/serve-index)
+      serveIndex: true,
+      // Can be:
+      // watch: {} (options for the `watch` option you can find https://github.com/paulmillr/chokidar)
+      watch: true,
+    },
     port: 3005,
-    publicPath: "/",
+    historyApiFallback: true,
+    proxy: {
+      '/api': 'http://localhost:5000'
+    },
     // hotOnly: true,
     hot: true,
     open: true,
-    proxy: {
-      '/api': 'http://localhost:5900',
-    },
-    historyApiFallback: true
   },
   cache: {
     type: 'filesystem',
