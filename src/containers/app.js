d80d12eed8f66bb763942174327579d198f19754
keep_wan <whevether@outlook.com>
2021-11-11 02:30:17 +0800

    update

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Routes,Route } from 'react-router-dom';
+import {/*Routes, Route*/ useRoutes } from 'react-router-dom';
 import { connect } from 'react-redux';
 //总布局
 import RootLayout from './rootLayout';
@@ -18,38 +18,77 @@ import NotFound from '../components/nodeFound';
 import Error401 from '../components/401';
 import Keep from '../components/keep';
 import Login from '../components/login';
-/* 相当于 v4 v5中的switch路由 */
-const ConnectedSwitch = connect(state => ({
-  state: state
-}))(Routes);
+/* Routes 相当于 v4 v5中的switch路由 */
+const routes = [{
+  path: "/",
+  element: <RootLayout />,
+  children:[{
+    element: <DefaultLayout />,
+    children: [{
+      index: true,
+      element: <Home />
+    },{
+      path: 'redbull',
+      element: <RedBull />
+    },{
+      path: 'snacks',
+      element: <Snacks />,
+      children: [{
+        path: 'one',
+        index: true,
+        element: <One />
+      },{
+        path: 'two',
+        element: <Two />
+      }]
+    },{
+      path: 'keep',
+      element: <Keep />
+    }]
+  },{
+    path: 'login',
+    element: <LoginLayout />,
+    children: [{
+      index: true,
+      element: <Login />
+    }]
+  },{
+    path: '401',
+    element: <Error401 />
+  },{
+    path: '*',
+    element: <NotFound />
+  }]
+}];
 // react路由.
 const App = () => {
   return (
     <div className="main" >
-      <ConnectedSwitch>
-        {/* 根布局 */}
+      {useRoutes(routes)}
+      {/* <Routes>
+        // 根布局
         <Route path="/" element={<RootLayout />}>
-          {/* 默认布局 */}
+          // 默认布局
           <Route element={<DefaultLayout />}>
             <Route index element={<Home />} />
             <Route element={<RedBull />} path="redbull"/>
-            {/* 子陆游 */}
+            // 子路由
             <Route path="snacks" element={<Snacks />}>
               <Route path="one" element={<One />} />
               <Route path="two" element={<Two />} />
             </Route>
             <Route element={<Keep />} path="keep"/>
           </Route>
-          {/* 登录页面布局 */}
+          // 登录页面布局
           <Route element={<LoginLayout />}>
             <Route element={<Login />} path="login" index/>
           </Route>
-          {/* 401页面 */}
+          // 401页面
           <Route element={<Error401 />} path="401"/>
-          {/* 没有找到页面 */}
+          // 没有找到页面
           <Route path="*" element={<NotFound />} />
         </Route>
-      </ConnectedSwitch>
+      </Routes> */}
     </div>
   );
 };
