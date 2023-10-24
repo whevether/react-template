5a76558675db317cab604701072f59b45b55483d
wanwenfeng <wanwenfeng@qq.com>
2023-10-24 09:47:07 +0800

    修改优化vite配置

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ import AutoImport from "unplugin-auto-import/vite";
 import viteCompression from "vite-plugin-compression"
 import atImport from 'postcss-import';
 import autoprefixer from 'autoprefixer';
-
 const { resolve } = path
 // const env = loadEnv(mode, process.cwd())
 // 打包模式
@@ -19,15 +18,15 @@ export default defineConfig({
   base: '/',
   publicDir: 'public',
   resolve: {
-    alias: [{find: 'components',replacement: path.resolve(__dirname, 'src/components/')},
-    {find: 'constants',replacement: path.resolve(__dirname, 'src/store/constants/')},
-    {find: 'actions',replacement: path.resolve(__dirname, 'src/store/actions/')},
-    {find: 'reducers',replacement: path.resolve(__dirname, 'src/store/reducers/')},
-    {find: 'router',replacement: path.resolve(__dirname, 'src/router/')},
-    {find: 'style',replacement: path.resolve(__dirname, 'src/style/')},
-    {find: 'store',replacement: path.resolve(__dirname, 'src/store/')},
-    {find: 'utils',replacement: path.resolve(__dirname, 'src/utils/')},
-    {find: 'assets',replacement:  path.resolve(__dirname, 'src/public/assets/')},
+    alias: [{ find: 'components', replacement: path.resolve(__dirname, 'src/components/') },
+    { find: 'constants', replacement: path.resolve(__dirname, 'src/store/constants/') },
+    { find: 'actions', replacement: path.resolve(__dirname, 'src/store/actions/') },
+    { find: 'reducers', replacement: path.resolve(__dirname, 'src/store/reducers/') },
+    { find: 'router', replacement: path.resolve(__dirname, 'src/router/') },
+    { find: 'style', replacement: path.resolve(__dirname, 'src/style/') },
+    { find: 'store', replacement: path.resolve(__dirname, 'src/store/') },
+    { find: 'utils', replacement: path.resolve(__dirname, 'src/utils/') },
+    { find: 'assets', replacement: path.resolve(__dirname, 'src/public/assets/') },
     { find: '@', replacement: resolve(__dirname, './src') }],
     extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
@@ -54,7 +53,7 @@ export default defineConfig({
   plugins: [
     reactRefresh(),
     AutoImport({
-      imports: ["react","react-router-dom"],
+      imports: ["react", "react-router-dom"],
     }),
     (modeEnv === 'production') && viteCompression({
       filter: /\.(js|css)$/i,
@@ -62,40 +61,42 @@ export default defineConfig({
       disable: false,
       threshold: 10240,
       algorithm: "gzip",
+      deleteOriginFile: true
       // ext: ".gz",
     }),
     createHtmlPlugin({
-    minify: true,
-    /**
-     * 在这里写entry后，你将不需要在`index.html`内添加 script 标签，原有标签需要删除
-     * @default src/main.ts
-     */
-    entry: 'index.jsx',
-    /**
-     * 如果你想将 `index.html`存放在指定文件夹，可以修改它，否则不需要配置
-     * @default index.html
-     */
-    template: 'index.html',
+      minify: true,
+      /**
+       * 在这里写entry后，你将不需要在`index.html`内添加 script 标签，原有标签需要删除
+       * @default src/main.ts
+       */
+      entry: 'index.jsx',
+      /**
+       * 如果你想将 `index.html`存放在指定文件夹，可以修改它，否则不需要配置
+       * @default index.html
+       */
+      template: 'index.html',
 
-    /**
-     * 需要注入 index.html ejs 模版的数据
-     */
-    inject: {
-      // data: {
-      //   title: 'index',
-      //   injectScript: `<script src="./index.js"></script>`,
-      // },
-      tags: [
-        {
-          injectTo: 'body-prepend',
-          tag: 'div',
-          attrs: {
-            id: 'tag',
+      /**
+       * 需要注入 index.html ejs 模版的数据
+       */
+      inject: {
+        minify: true,
+        // data: {
+          // title: 'react 模版',
+          // injectScript: `<script src="./index.js"></script>`,
+        // },
+        tags: [
+          {
+            injectTo: 'body-prepend',
+            tag: 'div',
+            attrs: {
+              id: 'tag',
+            },
           },
-        },
-      ],
-    },
-  })],
+        ],
+      },
+    })],
   define: {
     'process.env': {
     }
@@ -111,23 +112,38 @@ export default defineConfig({
         drop_debugger: true
       }
     },
+    reportCompressedSize: (modeEnv === 'production') ? false : true,
+    sourcemap: (modeEnv === 'production') ? false : true,
     rollupOptions: {
-      input:{
+      input: {
         app: path.resolve(__dirname, 'src/index.html')
       },
+      // external: ['react','react-dom'],
       output: {
-        entryFileNames: `js/[name]-[hash].js`,
-        chunkFileNames: `js/[name]-[hash].js`,
-        assetFileNames: assetInfo => {
-          var extType = 'assets';
-          if (
-            /\.(css|scss|sass|less)(\?.*)?$/i.test(assetInfo.name)
-          ) {
-            extType = 'css'
-          }
-          return `${extType}/[name]-[hash][extname]`
-        },
+        // globals: {
+        //   react: "React",
+        //   "react-dom": "ReactDOM",
+        // },
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }},
+          entryFileNames: `js/[name]-[hash].js`,
+            chunkFileNames: `js/[name]-[hash].js`,
+              assetFileNames: assetInfo => {
+                var extType = 'assets';
+                if (
+                  /\.(css|scss|sass|less)(\?.*)?$/i.test(assetInfo.name)
+                ) {
+                  extType = 'css'
+                }
+                return `${extType}/[name]-[hash][extname]`
+              },
+      },
       },
     },
-  },
-});
+  });
